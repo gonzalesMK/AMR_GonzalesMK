015ac2d6ca242f499387dbf20e836ee33089faee
GonzalesMK <judcnegri@gmail.com>
2016-03-20 12:21:58 -0300

    third commit

--- src/kineControl.cpp
+++ src/kineControl.cpp
@@ -1,6 +1,20 @@
 #include <ros/ros.h>
-
 #include <std_msgs/Float64.h>
+#include <geometry_msgs/Twist.h>
+
+    std_msgs::Float64 Vr;
+    std_msgs::Float64 Vl;
+    ros::Publisher R_Motor;
+    ros::Publisher L_Motor; 
+
+void callback(const geometry_msgs::TwistConstPtr& vel)
+{               
+        Vr.data = vel->linear.x + (0.35/2) * vel->angular.z ;
+        Vl.data = vel->linear.x - (0.35/2) * vel->angular.z ;
+        R_Motor.publish(Vr);
+        L_Motor.publish(Vl);        
+    
+}
 
 int main( int argc, char **argv)
 {
@@ -8,30 +22,9 @@ int main( int argc, char **argv)
     
     ros::NodeHandle node;
     
-    ros::Publisher R_Motor = node.advertise<std_msgs::Float64>("/vrep/vehicle/motorRightSpeed" , 1);   //ADICIONTAR O NOME TOPICO CORRETO
-    ros::Publisher L_Motor = node.advertise<std_msgs::Float64>("/vrep/vehicle/motorLeftSpeed" , 1 );
-    
-    ros::Rate LoopRate(10);
-    
-    std_msgs::Float64 Vr;
-    std_msgs::Float64 Vl;
-    std_msgs::Float64 V ;
-    std_msgs::Float64 W ;
-    
-    V.data = 1;
-    W.data = 0;
-    
-    while(ros::ok()) {
-        
-        Vr.data = V.data + (0.35/2) * W.data ;
-        Vl.data = V.data - (0.35/2) * W.data ;
-        
-        R_Motor.publish(Vr);
-        L_Motor.publish(Vl);        
-            
-        ros::spinOnce();
-        LoopRate.sleep();
-    }
-    
-    return 0;
+    R_Motor = node.advertise<std_msgs::Float64>("/vrep/vehicle/motorRightSpeed" , 1);   
+    L_Motor = node.advertise<std_msgs::Float64>("/vrep/vehicle/motorLeftSpeed" , 1 );
+    ros::Subscriber s = node.subscribe("/sonarController",1, callback); 
+     
+    ros::spin();
 }   
